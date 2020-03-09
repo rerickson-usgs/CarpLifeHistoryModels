a5cada00708e5c29e0a57b35a62db65e63c76057
Richard Erickson <rerickson@usgs.gov>
2020-03-09 10:50:35 -0500

    updtaed examine files

--- vonB/examine_silver_sex.R
+++ vonB/examine_silver_sex.R
@@ -1,13 +1,27 @@
-load("vonBfitNot0_silver.RData")
+load("vonBfitNot0_silver_female.RData")
+ls()
+
+
 
 ## stanOutO_SVCP
-stanOutOsummary_SVCP <-
-    summary(stanOutO_SVCP, probs = c(0.025, 0.1, 0.50, 0.9, 0.975))
-stanOutOsummary_SVCP[[1]][grepl("M", rownames(summary(stanOutO_SVCP)[[1]])), ]
+stanOutOsummary_SVCP_female <-
+    summary(stanOutO_silver_female, probs = c(0.025, 0.1, 0.50, 0.9, 0.975))
 
-stanOutOsummary_SVCP[[1]][grepl("bar", rownames(summary(stanOutO_SVCP)[[1]])), ]
-stanOutOsummary_SVCP[[1]][grepl("K", rownames(summary(stanOutO_SVCP)[[1]])), ]
-stanOutOsummary_SVCP[[1]][grepl("Linf", rownames(summary(stanOutO_SVCP)[[1]])),]
+plot(stanOutO_silver_female, pars =c("Linf_bar", "K_bar", "Linf"))
+print(stanOutO_silver_female, pars =c("Linf_bar", "K_bar", "Linf"))
 
 
-plot(stanOutO_SVCP, pars =c("Linf_bar", "K_bar", "Linf"))
+load("vonBfitNot0_silver_male.RData")
+x11()
+plot(stanOutO_silver_male, pars =c("Linf_bar", "K_bar", "Linf"))
+print(stanOutO_silver_male, pars =c("Linf_bar", "K_bar", "Linf"))
+
+
+print(stanOutO_silver_female, pars =c( "K_bar", "Linf_bar"))
+print(stanOutO_silver_male, pars =c( "K_bar", "Linf_bar"))
+
+plot(stanOutO_silver_female, pars =c("Linf_bar", "K_bar")) +
+    xlim(c(0.3, 1.1))
+x11()
+plot(stanOutO_silver_male, pars =c("Linf_bar", "K_bar")) +
+    xlim(c(0.3, 1.1))
